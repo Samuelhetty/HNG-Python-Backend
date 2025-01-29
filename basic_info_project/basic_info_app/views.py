91e97233eb29819d661a81cc24af00a377e6d46a
henrietta <hetty8004@gmail.com>
2025-01-29 07:17:58 -0800

    done

--- basic_info_project/basic_info_app/views.py
+++ basic_info_project/basic_info_app/views.py
@@ -11,9 +11,9 @@ def get_info(request):
 
     email_address = os.environ.get('EMAIL_ADDRESS', "hetty8004@gmail.com")
 
-    github_url = "https://github.com/samuelhetty"
+    github_url = "https://github.com/samuelhetty/HNG-Python-Backend"
 
-    current_datetime = datetime.now().isoformat()
+    current_datetime = datetime.now().isoformat() + "Z"
 
     data = {
         "email": email_address,
